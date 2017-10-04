c0cf5f9456b13ff245db768da8150c47d0b9cc79
raquelitarosaguilar <raquelita15@ru.is>
2017-10-04 11:31:20 +0000

    Added two functions: closeConnection and requestHandler

--- src/httpd.c
+++ src/httpd.c
@@ -1,20 +1,25 @@
-#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <ctype.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <glib.h>
-#include <sys/socket.h>
-#include <unistd.h>
 
 //Naming the server
 const char *SERVER_NAME = "localhost";
-const int server_port = 8800;
+
 
 // CONSTANTS
 
 // Maximum number of concurrent connections
-#define MAX_CONNECTIONS 10
+#define MAX_CONNECTIONS 30
 
 //FUNCTIONS
+
 // Handle the requests that the server receives
 void requestHandler(int socket);
 // Close the connection in slot number i
@@ -26,12 +31,25 @@ void closeConnection(int i);
 int connections[MAX_CONNECTIONS];
 //Monitor all connections that have been active in the server
 int countActiveConnections[MAX_CONNECTIONS];
+// Sockets
+int create_socket, socket_fd;
+// Server and client
+struct sockaddr_in server, client;
+
+// LOG FILE
+
+// Requirement: ,,In addition, for each request print a single line to a log file"
+FILE *log_fd;
+//File descriptor socket() returned
+int sockfd;
+fd_set connfds;
 
 
 
-void requestHandler(int i){
+void requestHandler(int){
     //initialise the buffer
     char message[4096];
+
     ssize_t n = recv(connections[i], message, sizeof(message), 0);
 
     if(n <= 0){
@@ -43,17 +61,31 @@ void requestHandler(int i){
         closeConnection(i);
         return;
     }
-    // bellow line was causing errors
-    // char message[4095];
+    char message[4095] = '\0';
 }
 
 // Close connection
 void closeConnection(int i){
-    shutdown(connections[i], SHUT_RDWR);
-    close(connections[i]);
-    connections[i] = 0;
+    socket_fd = shutdown(connfds, SHUT_RDWR);
+    if (socket_fd == -1){
+        perror("Shutdown");
+        exit(EXIT_FAILURE);
+    }
+    socket_fd = close(connfds);
+    if (socket_fd == -1){
+        prerror("Close");
+        exit(EXIT_FAILURE);
+    }
 }
+int main(){
+    char message[512];
 
-int main(int argc, char* argv[]){
-    return 0;
+    // Create and bind a socket
+    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0){
+        printf("New socket created\n");
+    }else if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Failed creating a new socket");
+        exit(EXIT_FAILURE);
+    }
+
 }

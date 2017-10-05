add781a8364bd9c4699a206ec00775a66044ce36
raquelitarosaguilar <raquelita15@ru.is>
2017-10-05 23:41:08 +0000

    Basic TCP structure that we can build on

--- src/httpd.c
+++ src/httpd.c
@@ -6,86 +6,110 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <glib.h>
-
-//Naming the server
-const char *SERVER_NAME = "localhost";
 
 
-// CONSTANTS
-
-// Maximum number of concurrent connections
-#define MAX_CONNECTIONS 30
+const char *LOG_FILE = "log_file.log";
+#define PORT 1500
 
 //FUNCTIONS
-
-// Handle the requests that the server receives
-void requestHandler(int socket);
-// Close the connection in slot number i
-void closeConnection(int i);
+void setup();
+void sendMessage();
+void closeConnection();
 
 //VARIBLES
-
-//Monitor all connections in the server
-int connections[MAX_CONNECTIONS];
-//Monitor all connections that have been active in the server
-int countActiveConnections[MAX_CONNECTIONS];
-// Sockets
-int create_socket, socket_fd;
-// Server and client
+char message[512];
+int sockfd, r;
 struct sockaddr_in server, client;
 
 // LOG FILE
-
-// Requirement: ,,In addition, for each request print a single line to a log file"
 FILE *log_fd;
-//File descriptor socket() returned
-int sockfd;
-fd_set connfds;
 
+void setup(){
 
-
-void requestHandler(int){
-    //initialise the buffer
-    char message[4096];
-
-    ssize_t n = recv(connections[i], message, sizeof(message), 0);
-
-    if(n <= 0){
-        if(n == 0){
-            printf("There is nothing from the socket to read\n");
-        }else{
-            printf("Failed to read from socket");
-        }
-        closeConnection(i);
-        return;
+    // Open log file
+    log_fd = fopen(LOG_FILE, "a");
+    if(log_fd == NULL){
+        printf("Failed to open the log file.\n");
     }
-    char message[4095] = '\0';
+
+    // Create sockets
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) > 0){
+        printf("New socket created\n");
+    }else if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Failed creating a new socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(PORT);
+
+    if(bind(sockfd, (struct sockaddr *) &server, sizeof(server)) == 0){
+        printf("Binding to socket");
+        exit(EXIT_FAILURE);
+    }
+
+    for (;;){
+        if(listen(sockfd, 10) < 0){
+            perror("Server listen");
+            exit(EXIT_FAILURE);
+        }
+
+        socklen_t len = (socklen_t) sizeof(client);
+        int connfd = accept(sockfd, (struct sockaddr *) &client, &len);
+        if (connfd < 0) {
+            perror("Server accept");
+            exit(EXIT_FAILURE);
+        }
+
+        if(r > 0){
+            printf("Connection has already been made to this client\n");
+        }
+
+        // Receive from connfd, not sockfd
+        ssize_t n = recv(connfd, message, sizeof(message) - 1, 0);
+        if (n < 0){
+            perror("Receiving message failed");
+            exit(EXIT_FAILURE);
+        }
+        message[n] = '\0';
+        fprintf(stdout, "Received: \n%s\n", message);
+
+        sendMessage();
+        closeConnection();
+    }
+}
+// send message
+void sendMessage(){
+
+    socklen_t len = (socklen_t) sizeof(client);
+    int connfd = accept(sockfd, (struct sockaddr *) &client, &len);
+    ssize_t n = recv(connfd, message, sizeof(message) - 1, 0);
+
+    r = send(connfd, message, (ssize_t) n, 0);
+    if(r < 0){
+        perror("Send");
+        exit(EXIT_FAILURE);
+    }
 }
 
-// Close connection
-void closeConnection(int i){
-    socket_fd = shutdown(connfds, SHUT_RDWR);
-    if (socket_fd == -1){
+void closeConnection(){
+    socklen_t len = (socklen_t) sizeof(client);
+    int connfd = accept(sockfd, (struct sockaddr *) &client, &len);
+    sockfd = shutdown(connfd, SHUT_RDWR);
+    if (sockfd == -1){
         perror("Shutdown");
         exit(EXIT_FAILURE);
     }
-    socket_fd = close(connfds);
-    if (socket_fd == -1){
-        prerror("Close");
+    sockfd = close(connfd);
+    if (sockfd == -1){
+        perror("Close");
         exit(EXIT_FAILURE);
     }
 }
 int main(){
-    char message[512];
 
-    // Create and bind a socket
-    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0){
-        printf("New socket created\n");
-    }else if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Failed creating a new socket");
-        exit(EXIT_FAILURE);
-    }
+
 
 }

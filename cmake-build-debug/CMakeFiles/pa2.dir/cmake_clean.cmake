bb0a925407fdf84acd228a53232e7bf417d69faf
raquelitarosaguilar <raquelita15@ru.is>
2017-10-08 13:52:07 +0000

    GET implemented, HEAD+PUT not done, able to create the logfile but not print into it

---
+++ cmake-build-debug/CMakeFiles/pa2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pa2.dir/src/httpd.c.o"
+  "pa2.pdb"
+  "pa2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pa2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/httpd.c
+++ src/httpd.c
@@ -1,24 +1,115 @@
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
-#include <ctype.h>
-#include <string.h>
-#include <unistd.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
 #include <glib.h>
 
+//61278
 
 const char *LOG_FILE = "log_file.log";
-int sockfd, connfd;
+FILE *log_fd;
+struct sockaddr_in serv_addr;
+struct sockaddr_in client_addr;
+int client_sock;
 
-void sighandler() {
-    // if SIGINT signal (Ctrl-C) then print
-    fprintf(stdout, "\nShutting down...\n");
-    // close connection
-    shutdown(connfd, SHUT_RDWR);
-    close(connfd);
+// FUNCTIONS
+void print_logfile();
+
+
+
+void error(char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+// GET request has be generated in memory, and should be a HTML 5 page.
+// Content should include:
+// 1. URL of the requested page
+// 2. IP address and port number of the requested client
+// When you visit a browser it should display this format:
+// http://foo.com/page 123.123.123.123:45678
+void write_get(int client_sock, struct sockaddr_in *client_addr, char *webpage) {
+
+    char term[] = "\r\n";
+    char body[4098];
+    memset(&body, 0, sizeof(char) * 4098);
+    strcat(body, "<!DOCTYPE html><html><head>");
+    strcat(body, "<title>Small Server</title>");
+    strcat(body, "</head>");
+    strcat(body, "<body>");
+    strcat(body, "<p>");
+
+    strcat(body, webpage);
+    strcat(body, " ");
+
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, client_addr, client_ip, INET_ADDRSTRLEN);
+    strcat(body, client_ip);
+
+    char client_port[16];
+    memset(&client_port, 0, sizeof(char) * 16);
+    sprintf(client_port, "%i", ntohs(client_addr->sin_port));
+    strcat(body, ":");
+    strcat(body, client_port);
+
+    strcat(body, "</p>");
+    strcat(body, "</body>");
+    strcat(body, "</html>");
+
+    char len[16];
+    memset(&len, 0, sizeof(char) * 16);
+    sprintf(len, "%i", (int)strlen(body));
+
+    char response[4098];
+    memset(&response, 0, sizeof(char) * 4098);
+    strcat(response, "HTTP/1.1 200 OK");
+    strcat(response, term);
+    strcat(response, "Content-Type: ");
+    strcat(response, "text/html");
+    strcat(response, term);
+    strcat(response, "Content-Length: ");
+    strcat(response, len);
+    strcat(response, term);
+    strcat(response, "\n");
+    strcat(response, body);
+    strcat(response, term);
+
+    printf("\nResponse\n%s\n", response);
+
+    if (write(client_sock, response, (int) strlen(response)) == -1) {
+        error("ERROR writing to socket");
+    }
+}
+
+// PUT requests, the page has to be generated in memory and should be a HTML 5 page.
+// Content should include:
+// 1. URL of the requested page
+// 2. IP address and PORT of the requesting client,
+// 3. the data in the body of the PUT request
+void write_head(int client_sock) {
+    // TODO: Add any extra parameter
+    // Return: HTTP/1.1 200 OK
+}
+
+// HEAD request, just generate the header of the requested page.
+void write_put(int client_sock) {
+    // TODO: Add any extra parameter
+    // PUT is similar to the GET but instead of the body
+    // you have to put the body of the request they sent
+}
+
+// Print out to the log file
+void print_logfile(){
+
+    int status = 200;
+    GTimeVal time;
+    g_get_current_time(&time);
+    time.tv_sec = 0;
+    gchar *now = g_time_val_to_iso8601(&time);
+    fprintf(log_fd, ": %s %s %d\n", now, inet_ntoa(client_addr.sin_addr), status);
 }
 
 int main(int argc, char *argv[]) {
@@ -28,108 +119,75 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "expected usage: %s <port> \n", argv[0] );
         exit(0);
     }
-    // set portnumber:
+    // set portnumber: test
     int PORT = atoi(argv[1]);
     fprintf(stdout, "Listening on port %d\n", PORT);
 
-    signal(SIGINT, sighandler);
-
-    int r;
-    struct sockaddr_in server, client;
-    char message[512];
-
-    // Create and bind a TCP socket.
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        perror("socket");
+    // Open the log file
+    log_fd = fopen(LOG_FILE, "a");
+    if(log_fd == NULL){
+        printf("Failed to open the log file\n");
         exit(EXIT_FAILURE);
     }
 
-    // Network functions need arguments in network byte order instead of
-    // host byte order. The macros htonl, htons convert the values.
-    memset(&server, 0, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(PORT);
-    r = bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
-    if (r == -1) {
-        perror("bind");
+    print_logfile();
+
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock == -1) {
+        perror("Failed to create a socket");
         exit(EXIT_FAILURE);
     }
 
-    // Before the server can accept messages, it has to listen to the
-    // welcome port. A backlog of one connection is allowed.
-    r = listen(sockfd, 1);
-    if (r == -1) {
-        perror("listen");
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+
+    if (bind(server_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
+        perror("ERROR: Failed to bind socket\n");
         exit(EXIT_FAILURE);
     }
 
-    for (;;) {
-        fprintf(stdout, "Waiting for connection...\n");
+    listen(server_sock, 10);
 
-        // We first have to accept a TCP connection, connfd is a fresh
-        // handle dedicated to this connection.
-        socklen_t len = (socklen_t) sizeof(client);
-        connfd = accept(sockfd, (struct sockaddr *) &client, &len);
-        if (connfd == -1) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
 
-        fprintf(stdout, "TCP connection accepted\n");
+    memset(&client_addr, 0, sizeof(client_addr));
+    socklen_t client_size = sizeof(client_addr);
+    client_sock = accept(server_sock, (struct sockaddr *) &client_addr, &client_size);
+    if (client_sock == -1) {
+        perror("ERROR: Connection not accpted\n");
+        exit(EXIT_FAILURE);
+    }
 
-        // Receive from connfd, not sockfd.
-        ssize_t n = recv(connfd, message, sizeof(message) - 1, 0);
-        if (n == -1) {
-            perror("recv");
-            exit(EXIT_FAILURE);
-        }
+    char request[4098];
+    memset(&request, 0, sizeof(char) * 4098);
 
-	fprintf(stdout, "received from connfd\n");
-        message[n] = '\0';
-        fprintf(stdout, "Received:\n%s\n", message);
+    if (read(client_sock, request, 4097) == -1) {
+        perror("ERROR: Failed to read from socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("\nRequest\n%s\n", request);
 
-        //gchar **headers = g_strsplit(message, "\n", -1);
-        /*
-         split message into lines
-         each line is going to contain an http line (header, content type, content length, etc...)
-         search for the http method which is always the first line and see which one is (get, post, etc...)
-         construct the response depending on what method you got
+    gchar **split = g_strsplit(request, "\n", -1);
+    gchar **first = g_strsplit(*split, " ", -1);
 
-         example for get write
+    // Check which response the server gets and do
+    // the appropriate functionality
+    if (strcmp(first[0], "GET") == 0) {
+        write_get(client_sock, &client_addr, first[1]);
+    } else if (strcmp(first[0], "PUT") == 0) {
+        write_put(client_sock); // Add any extra parameter
+    } else if (strcmp(first[0], "HEAD") == 0) {
+        write_head(client_sock); // Add any extra parameter
+    }
 
-         "GET HTTP/1.1
-         Content-Type: text/html
-         Content-Length: 35
-
-         <html>http://www.raquelita.com ip</html>"
-        */
-
-        // Send the message back.
-
-        char response[4096];
-        memset(response, 0, sizeof(char) * 4096);
-        strcat(response, "GET HTTP/1.1\r\nContent-Type: text/html\r\nContent-Length: 35\r\n<html>http://www.raquelita.com ");
-        strcat(response, "127.0.0.1:6525");
-        strcat(response, "</html>");
-
-        r = send(connfd, response, (size_t) sizeof(char) * strlen(response), 0);
-        if (r == -1) {
-            perror("send");
-            exit(EXIT_FAILURE);
-        }
-
-        // Close the connection.
-        r = shutdown(connfd, SHUT_RDWR);
-        if (r == -1) {
-            perror("shutdown");
-            exit(EXIT_FAILURE);
-        }
-        r  = close(connfd);
-        if (r == -1) {
-            perror("close");
-            exit(EXIT_FAILURE);
-        }
+    // Close the connection.
+    if (shutdown(client_sock, SHUT_RDWR) == -1) {
+        perror("Closing socket");
+        exit(EXIT_FAILURE);
+    }
+    if (close(client_sock) == -1) {
+        perror("Closing socket");
+        exit(EXIT_FAILURE);
     }
 }

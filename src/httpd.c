f52fd6aeb7d5b5b138c305b45ab6b244d2b826e8
raquelitarosaguilar <raquelita15@ru.is>
2017-10-10 18:53:13 +0000

    GET should be done + ode clean

--- src/httpd.c
+++ src/httpd.c
@@ -1,3 +1,4 @@
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -7,16 +8,18 @@
 #include <string.h>
 #include <glib.h>
 
-//61278
 
+//61278
 const char *LOG_FILE = "log_file.log";
 FILE *log_fd;
 struct sockaddr_in serv_addr;
 struct sockaddr_in client_addr;
 int client_sock;
+int port;
 
 // FUNCTIONS
 void print_logfile();
+void closeConnection();
 
 // GET request has be generated in memory, and should be a HTML 5 page.
 // Content should include:
@@ -29,14 +32,22 @@ void write_get(int client_sock, struct sockaddr_in *client_addr, char *webpage) 
     char term[] = "\r\n";
     char body[4098];
     memset(&body, 0, sizeof(char) * 4098);
-    strcat(body, "<!DOCTYPE html><html><head>");
-    strcat(body, "<title>Small Server</title>");
+    strcat(body, "<!DOCTYPE html><html><head><meta charset=\"UTF-8\">");
+    strcat(body, "<title>PA2_SERVER</title>");
     strcat(body, "</head>");
     strcat(body, "<body>");
     strcat(body, "<p>");
+    strcat(body, "<h1>");
+    strcat(body, "<b>");
 
+    char string_port[16];
+    memset(&string_port, 0, sizeof(char) * 16);
+    sprintf(string_port, "%i", port);
+    strcat(body, "http://localhost:");
+    strcat(body, string_port);
     strcat(body, webpage);
     strcat(body, " ");
+
 
     char client_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, client_addr, client_ip, INET_ADDRSTRLEN);
@@ -48,6 +59,8 @@ void write_get(int client_sock, struct sockaddr_in *client_addr, char *webpage) 
     strcat(body, ":");
     strcat(body, client_port);
 
+    strcat(body, "</b>");
+    strcat(body, "</h1>");
     strcat(body, "</p>");
     strcat(body, "</body>");
     strcat(body, "</html>");
@@ -77,39 +90,57 @@ void write_get(int client_sock, struct sockaddr_in *client_addr, char *webpage) 
     }
 }
 
-// PUT requests, the page has to be generated in memory and should be a HTML 5 page.
-// Content should include:
-// 1. URL of the requested page
-// 2. IP address and PORT of the requesting client,
-// 3. the data in the body of the PUT request
-void write_head(int client_sock) {
-    // TODO: Add any extra parameter
-    // Return: HTTP/1.1 200 OK
+// HEAD request, just generate the header of the requested page.
+// TODO: Return: HTTP/1.1 200 OK
+void write_head(int client_sock) { // Add any extra parameter
     /*char response[1024];
     (void)file;
     strcat(response, "HTTP/1.0 200 OK\r\n");
     printf(client_sock, response, strlen(response), 0);*/
 }
 
-// HEAD request, just generate the header of the requested page.
-void write_put(int client_sock) {
-    // TODO: Add any extra parameter
+// PUT requests, the page has to be generated in memory and should be a HTML 5 page.
+// Content should include:
+// 1. URL of the requested page
+// 2. IP address and PORT of the requesting client,
+// 3. the data in the body of the PUT request
+// TODO:
+void write_put(int client_sock) { // Add any extra parameter
     // PUT is similar to the GET but instead of the body
     // you have to put the body of the request they sent
 }
 
-// Print out to the log file
-void print_logfile() {
+// print the data to a single line into a log file
+void print_logfile(){
+
     int status = 200;
     GTimeVal time;
     g_get_current_time(&time);
-    time.tv_sec = 0;
+    time.tv_usec = 0;
     gchar *now = g_time_val_to_iso8601(&time);
-    fprintf(log_fd, ": %s %s %d\n", now, inet_ntoa(client_addr.sin_addr), status);
-    fputs(log_fd, inet_ntoa(client_addr.sin_addr));
+
+    // Print to the log file
+    fprintf(log_fd, "%s : %s:%d\n", now, inet_ntoa(client_addr.sin_addr), status);
+    printf("%s : %s:%d\n", now, inet_ntoa(client_addr.sin_addr), status);
+    g_free(now);
+    fprintf(log_fd, "%s", inet_ntoa(client_addr.sin_addr));
     fclose(log_fd);
-    close(client_sock);
-    // This is not finished cause there is nothing written in the file yet
+    //close(client_sock);
+
+}
+
+// Close the connection.
+void closeConnection(){
+    if (close(client_sock) == -1) {
+        perror("Closing socket");
+        exit(EXIT_FAILURE);
+    }
+    if (0 && shutdown(client_sock, SHUT_RDWR) == -1) {
+        //I wrote 0 && shutdown so the shutdown function does not run because it always fails.
+        // I don't know how to use this properly
+        perror("Shutdown socket");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -120,8 +151,8 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
     // set portnumber: test
-    int PORT = atoi(argv[1]);
-    fprintf(stdout, "Listening on port %d\n", PORT);
+    port = atoi(argv[1]);
+    fprintf(stdout, "Listening on port %d\n", port);
 
     // Open the log file
     log_fd = fopen(LOG_FILE, "a");
@@ -141,7 +172,7 @@ int main(int argc, char *argv[]) {
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT); // Connect to server
+    serv_addr.sin_port = htons(port); // Connect to server
 
     if (bind(server_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
         perror("ERROR: Failed to bind socket\n");
@@ -155,7 +186,7 @@ int main(int argc, char *argv[]) {
     socklen_t client_size = sizeof(client_addr);
     client_sock = accept(server_sock, (struct sockaddr *) &client_addr, &client_size);
     if (client_sock == -1) {
-        perror("ERROR: Connection not accpted\n");
+        perror("ERROR: Connection not accepted\n");
         exit(EXIT_FAILURE);
     }
 
@@ -171,8 +202,7 @@ int main(int argc, char *argv[]) {
     gchar **split = g_strsplit(request, "\n", -1);
     gchar **first = g_strsplit(*split, " ", -1);
 
-    // Check which response the server gets and do
-    // the appropriate functionality
+    // Checking which response the server gets
     if (strcmp(first[0], "GET") == 0) {
         write_get(client_sock, &client_addr, first[1]);
     } else if (strcmp(first[0], "PUT") == 0) {
@@ -180,25 +210,5 @@ int main(int argc, char *argv[]) {
     } else if (strcmp(first[0], "HEAD") == 0) {
         write_head(client_sock); // Add any extra parameter
     }
-
-    // If time runs out
-    int connection[10];
-    int active[10];
-    for (int i = 0; i < 10; ++i) {
-        if(connection[i] != 0){
-            if(g_get_monotonic_time() - active[i] > TIME_INACTIVE){
-                printf("Connection ran out of time\n", i);
-            }
-        }
-    }
-
-    // Then close the connection.
-    if (shutdown(client_sock, SHUT_RDWR) == -1) {
-        perror("Closing socket");
-        exit(EXIT_FAILURE);
-    }
-    if (close(client_sock) == -1) {
-        perror("Closing socket");
-        exit(EXIT_FAILURE);
-    }
+    closeConnection();
 }
